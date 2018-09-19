9f9819e8912d4ff24cf51f922eb0a20ac5a6987d
ElwinBran <elwinbran@gmail.com>
2018-09-19 08:21:02 +0200

    Progress dump. The app does not seem to contain fatal problems and the core gamble logic has been implemented

---
+++ app/src/main/java/com/github/elwinbran/mad/gamble/DiceRollAdapter.java
@@ -0,0 +1,55 @@
+package com.github.elwinbran.mad.gamble;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class DiceRollAdapter extends RecyclerView.Adapter<DiceRollAdapter.DiceRollViewHolder> {
+
+    private final Context context;
+    private final List data;
+
+    public DiceRollAdapter(Context context, List data)
+    {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public DiceRollViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(this.context).inflate(R.layout.activity_main,parent,false);
+        DiceRollViewHolder vh = new DiceRollViewHolder(v, context);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiceRollViewHolder holder, int position) {
+        holder.textView.setText(data.get(position).toString());
+        Toast.makeText(this.context,"Tayce.T",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class DiceRollViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView textView;
+        public DiceRollViewHolder(View viewItem, Context context)
+        {
+            super(viewItem);
+            textView = new TextView(context);
+        }
+    }
+}
--- app/src/main/java/com/github/elwinbran/mad/gamble/MainActivity.java
+++ app/src/main/java/com/github/elwinbran/mad/gamble/MainActivity.java
@@ -1,13 +1,154 @@
 package com.github.elwinbran.mad.gamble;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer lastRollResult = 0;
+
+    private Integer streak = 0;
+
+    private List<Integer> rollHistory = new ArrayList<>(50);
+
+    private Integer highscore = 0;//TODO get from external persistance.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        roll();
+        RecyclerView log = findViewById(R.id.recyclerView);
+        log.setAdapter(new DiceRollAdapter(getApplicationContext(), this.rollHistory));
+        SharedPreferences prefs = getSharedPreferences("UserData", MODE_PRIVATE);
+        highscore = prefs.getInt("highscore", 0); // 0 is default
+        //lower bet button
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Integer newNumber = roll();
+                int comparison = getLastRollResult().compareTo(newNumber);
+                logRoll(newNumber);
+                if(comparison > 0)
+                {
+                    won();
+
+                }
+                else
+                {
+                    lost();
+                }
+                setLastRollResult(newNumber);
+            }
+        });
+
+        //higher bet button
+        final Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Integer newNumber = roll();
+                int comparison = getLastRollResult().compareTo(newNumber);
+                logRoll(newNumber);
+                if(comparison < 0)
+                {
+                    won();
+                }
+                else
+                {
+                    lost();
+                }
+                setLastRollResult(newNumber);
+            }
+        });
+    }
+
+    /**
+     * updates and returns new number.
+     * @return
+     */
+    private Integer roll()
+    {
+        Integer newNumber = diceRoll();
+        ImageView diceDisplay = findViewById(R.id.imageView);
+        switch(newNumber){
+            case 1: diceDisplay.setImageResource(R.drawable.d1);
+                break;
+            case 2: diceDisplay.setImageResource(R.drawable.d2);
+                break;
+            case 3: diceDisplay.setImageResource(R.drawable.d3);
+                break;
+            case 4: diceDisplay.setImageResource(R.drawable.d4);
+                break;
+            case 5: diceDisplay.setImageResource(R.drawable.d5);
+                break;
+            case 6: diceDisplay.setImageResource(R.drawable.d6);
+                break;
+            default: diceDisplay.setImageResource(R.drawable.dinit);
+                break;
+        }
+        return newNumber;
+    }
+
+    private void won()
+    {
+        streak++;
+        updateStreakDisplay();
+        updateHighscore();
+    }
+
+    private void lost()
+    {
+        streak = 0;
+        updateStreakDisplay();
+    }
+
+    private void logRoll(Integer rollResult)
+    {
+        this.rollHistory.add(rollResult);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    private void updateStreakDisplay()
+    {
+        TextView streakDisplay = findViewById(R.id.textView);
+        streakDisplay.setText("current streak: " + streak.toString());
+    }
+
+    private void updateHighscore()
+    {
+        if(streak > highscore) {
+            highscore = streak;
+            SharedPreferences.Editor editor = getSharedPreferences("UserData", MODE_PRIVATE).edit();
+            editor.putInt("highscore", highscore);
+            editor.apply();
+        }
+    }
+
+    private static Integer diceRoll()
+    {
+        final int lowestNumber = 1;
+        final int highestNumber = 6;
+        Random r = new Random();
+        return r.nextInt((highestNumber + 1) - lowestNumber) + lowestNumber;
+    }
+
+    public Integer getLastRollResult()
+    {
+        return this.lastRollResult;
+    }
+
+    public void setLastRollResult(Integer newRollResult)
+    {
+        this.lastRollResult = newRollResult;
     }
 }
